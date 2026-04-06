783bd83d0f957e22074f19a83da020c9bfae1a67
impaulinha <anaaguiar20016@gmail.com>
2026-04-06 15:46:48 -0300

    chore: criada constants dos planetas

---
+++ src/types/planet.ts
@@ -0,0 +1,23 @@
+export type IPlanetColor =
+  | 'planet-mercury'
+  | 'planet-venus'
+  | 'planet-earth'
+  | 'planet-mars'
+  | 'planet-jupiter'
+  | 'planet-saturn'
+  | 'planet-uranus'
+  | 'planet-neptune'
+
+export interface IPlanet {
+  id: string
+  index: number
+  name: string
+  apiId: string
+  nasaQuery: string
+  texture: string
+  color: IPlanetColor
+  ringTexture?: string
+  description: string
+}
+
+export type IPlanetId = string
