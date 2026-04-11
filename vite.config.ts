92c06a8f75dbff3646d83279ac78b3329865d8be
impaulinha <anaaguiar20016@gmail.com>
2026-04-11 04:07:13 -0300

    fix: proxy removido

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,9 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-
-  return {
-    plugins: [react(), tsconfigPaths(), tailwindcss()],
-    server: {
-      proxy: {
-        '/solar-api': {
-          target: 'https://api.le-systeme-solaire.net',
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/solar-api/, '/rest'),
-          headers: {
-            Authorization: `Bearer ${env.VITE_SOLAR_API_KEY}`,
-          },
-        },
-      },
-    },
-  }
+export default defineConfig({
+  plugins: [react(), tsconfigPaths(), tailwindcss()],
+  server: { open: true },
 })
